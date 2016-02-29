449e65be80f3b90d0ffc3b75236021919d12ef4e
Vaibhav Vishal <vaibhavvishal2@gmail.com>
2016-02-29 17:00:40 +0530

    core java basics

---
+++ src/com/training/Greeting.java
@@ -0,0 +1,9 @@
+package com.training;
+
+public class Greeting {
+
+public String getMessage() {
+	return "welcome to java";
+}
+	
+}
---
+++ src/com/training/PrimCasting.java
@@ -0,0 +1,19 @@
+package com.training;
+
+public class PrimCasting {
+
+	public Integer add(Integer a , Integer b)
+	{
+		Integer c = a+b;
+		
+		return c+100;
+	}
+	
+	public int add(int a , int b)
+	{
+	  return a+b;	
+	}
+
+
+}
+
